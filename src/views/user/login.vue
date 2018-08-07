04d8d79b1541582a9f3164f1c0af645249d0ce74
cj <527781599@qq.com>
2018-08-07 18:54:51 +0800

    增加登录权限

--- src/views/user/login.vue
+++ src/views/user/login.vue
@@ -1,6 +1,12 @@
 <template>
-    <div>
+    <div class="wid-100 hei-100">
         <header-nav navTitle="登录"></header-nav>
+        <div class="app-sub-content hei-100 flex flex-trans-center flex-vert-center flex-column">
+          <h1 class="block">张马旅游</h1>
+          <input type="text" class="block wid-100" placeholder="用户名">
+          <input type="password" class="block wid-100" placeholder="密码">
+          <div class="btn btn-info">登录</div>
+        </div>
     </div>
 </template>
 
@@ -12,3 +18,12 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.app-sub-content{
+  padding: 0 15px;
+}
+h1{
+  font-size: 24px;
+}
+</style>
