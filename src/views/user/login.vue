883ee5031cc2ae0445f57bf91613471e3a3a2434
cj <527781599@qq.com>
2018-08-07 22:47:32 +0800

    完善登录

--- src/views/user/login.vue
+++ src/views/user/login.vue
@@ -3,9 +3,9 @@
         <header-nav navTitle="登录"></header-nav>
         <div class="app-sub-content hei-100 flex flex-trans-center flex-vert-center flex-column">
           <h1 class="block">张马旅游</h1>
-          <input type="text" class="block wid-100" placeholder="用户名">
-          <input type="password" class="block wid-100" placeholder="密码">
-          <div class="btn btn-info">登录</div>
+          <input type="text" class="block wid-100" v-model="username" placeholder="用户名">
+          <input type="password" class="block wid-100" v-model="password" placeholder="密码">
+          <div class="btn btn-info wid-100" @click="login">登录</div>
         </div>
     </div>
 </template>
@@ -15,6 +15,17 @@ import HeaderNav from '@/components/header'
 export default {
   components: {
     HeaderNav
+  },
+  data () {
+    return {
+      username: '13164649520',
+      password: '111111'
+    }
+  },
+  methods: {
+    login () {
+
+    }
   }
 }
 </script>
@@ -25,5 +36,16 @@ export default {
 }
 h1{
   font-size: 24px;
+  margin-bottom: 50px;
+}
+input{
+  outline: none;
+  border: none;
+  border-bottom: solid 1px #ddd;
+  margin-bottom: 10px;
+  padding: 8px 0;
+}
+.btn{
+  margin-top: 20px;
 }
 </style>
